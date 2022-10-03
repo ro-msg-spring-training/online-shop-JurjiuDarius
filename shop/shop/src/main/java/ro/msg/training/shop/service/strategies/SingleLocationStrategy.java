e34b02c91f54ece3ea16ce263b1711db28cd5d94
Darius Jurjiu-Tandrau <Darius.Jurjiu-Tandrau@msg.group>
2022-10-03 09:35:27 +0300

    Fixed a problem with SingleLocationStrategy

--- shop/shop/src/main/java/ro/msg/training/shop/service/strategies/SingleLocationStrategy.java
+++ shop/shop/src/main/java/ro/msg/training/shop/service/strategies/SingleLocationStrategy.java
@@ -2,22 +2,20 @@ package ro.msg.training.shop.service.strategies;
 
 import ro.msg.training.shop.entity.Location;
 import ro.msg.training.shop.entity.OrderDetail;
-import ro.msg.training.shop.entity.Product;
 import ro.msg.training.shop.entity.Stock;
 import ro.msg.training.shop.service.LocationService;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class SingleLocationStrategy implements LocationStrategy {
 	
 	@Override
 	public ArrayList<Stock> orderLocationStrategy(LocationService locationService, ArrayList<OrderDetail> orderDetails) {
 		Iterable<Location> locations = locationService.getLocations();
-		HashMap<Product, Location> productLocations = new HashMap<>();
 		boolean validLocation;
 		ArrayList<Stock> stocks = new ArrayList<>();
 		for (Location location : locations) {
+			
 			validLocation = true;
 			for (OrderDetail orderDetail : orderDetails) {
 				for (Stock stock : location.getStocks()) {
@@ -30,10 +28,11 @@ public class SingleLocationStrategy implements LocationStrategy {
 			if (validLocation) {
 				for (OrderDetail orderDetail : orderDetails) {
 					Stock stock = new Stock();
-					stock.setLocation(productLocations.get(orderDetail.getProduct()));
+					stock.setLocation(location);
 					stock.setQuantity(orderDetail.getQuantity());
 					stock.setProduct(orderDetail.getProduct());
 					stocks.add(stock);
+					
 				}
 				return stocks;
 			}
