92cbe002b0a9efb2f58fe8dae18bd45d9245f6ba
Darius Jurjiu-Tandrau <Darius.Jurjiu-Tandrau@msg.group>
2022-10-04 16:55:26 +0300

    Finished implementing security, basic and form-based. The profiles can be added/removed in the application.properties file. The current commit has no added security.

--- shop/shop/src/main/java/ro/msg/training/shop/security/BasicSecurityConfiguration.java
+++ shop/shop/src/main/java/ro/msg/training/shop/security/BasicSecurityConfiguration.java
@@ -3,48 +3,40 @@ package ro.msg.training.shop.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import ro.msg.training.shop.entity.Customer;
 import ro.msg.training.shop.service.CustomerService;
 
-import java.util.List;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class BasicConfiguration {
+@Profile("basic")
+public class BasicSecurityConfiguration {
 	
 	@Autowired
 	private CustomerService customerService;
 	
 	@Bean
-	public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-		InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
-		List<Customer> customers = customerService.getCustomers();
-		for (Customer customer : customers) {
-			UserDetails user = User.withUsername(customer.getUsername()).password(passwordEncoder.encode(customer.getPassword())).roles("USER").build();
-			inMemoryUserDetailsManager.createUser(user);
-		}
-		
-		return inMemoryUserDetailsManager;
-	}
-	
-	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).httpBasic(withDefaults());
 		return http.build();
 	}
 	
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		return encoder;
+	public InMemoryUserDetailsManager userDetailsService() {
+		InMemoryUserDetailsManager detailsManager = new InMemoryUserDetailsManager();
+		for (Customer customer : customerService.getCustomers()) {
+			UserDetails user = User.withDefaultPasswordEncoder().username(customer.getUsername()).password(customer.getPassword()).roles("USER").build();
+			detailsManager.createUser(user);
+		}
+		return detailsManager;
 	}
 	
 }
